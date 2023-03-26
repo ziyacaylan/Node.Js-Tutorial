4dfa46e2667895658db72eef07e6d3b0830d766b
ziyacaylan <ziyacaylan@gmail.com>
2023-03-26 12:45:07 +0300

    patika ödev 7 tamamlandı

---
+++ PCAT/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  const photo = {
+    id: 1,
+    name: "Photo Name",
+    description: "Photo Description",
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
