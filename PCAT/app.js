674b090c5e4adc0632cc2fa46ccfeddf5c13cb70
ziyacaylan <ziyacaylan@gmail.com>
2023-03-27 12:01:31 +0300

    PCAT project static files uploaded

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+// MIDDLEWARES
+// const myLogger = (req, res, next) => {
+//   console.log("Middleware Log 1");
+//   next();
+// };
+
+// const myLogger2 = (req, res, next) => {
+//   console.log("Middleware Log 2");
+//   next();
+// };
+// app.use(myLogger);
+// app.use(myLogger2);
+
+app.use(express.static("temp"));
 
 app.get("/", (req, res) => {
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo Description",
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
 });
 
 const port = 3000;
