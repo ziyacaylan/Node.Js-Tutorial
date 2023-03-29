6e8662da32ffcf36eebc4aaf98741f35bd967878
ziyacaylan <ziyacaylan@gmail.com>
2023-03-29 11:16:48 +0300

    mongoose projeye dahil edildi ve db ile bağlantı kuruldu

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const ejs = require("ejs");
-const app = express();
 const path = require("path");
+const Photo = require("./models/Photo");
+
+const app = express();
+
+//connect DB
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // TEMLPATE ENGINE
 app.set("view engine", "ejs");
@@ -21,16 +31,25 @@ app.set("view engine", "ejs");
 
 // MIDDLEWARES
 app.use(express.static("temp"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", { photos });
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
 app.get("/add", (req, res) => {
   res.render("add");
+});
+app.post("/photos", async (req, res) => {
+  //res.render("add");
+  // console.log(req.body);
+  await Photo.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
